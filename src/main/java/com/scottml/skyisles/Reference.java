503592f2ee20fc8c5aa1c1abdb7d0db4a2b34fdc
Michael Scott <mike@scottml.com>
2018-12-16 14:29:59 -0800

    Initial Sky Isles Mod commit

---
+++ src/main/java/com/scottml/skyisles/Reference.java
@@ -0,0 +1,8 @@
+package com.scottml.skyisles;
+
+public class Reference {
+	public static final String MODID = "sim";
+	public static final String MODNAME = "Sky Isles Mod";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+}
---
+++ src/main/java/com/scottml/skyisles/items/ItemBasic.java
@@ -0,0 +1,11 @@
+package com.scottml.skyisles.items;
+
+import net.minecraft.item.Item;
+
+public class ItemBasic extends Item {
+	
+	public ItemBasic(String name) {
+		setUnlocalizedName(name);
+		setRegistryName(name);
+	}
+}
